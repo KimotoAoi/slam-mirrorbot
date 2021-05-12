0fe77e3aac63af73da74340b61ee776f16fafb45
KimotoAoi <55420858+KimotoAoi@users.noreply.github.com>
2021-05-13 06:47:19 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -151,7 +151,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Mirror Bot',
+            'description': 'Uploaded By Giang',
             'mimeType': mime_type,
         }
         if parent_id is not None:

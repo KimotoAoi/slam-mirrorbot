0638cffb8475a28de858974cfd411bcb9c0c2648
kimotoaoi <55420858+kimotoaoi@users.noreply.github.com>
2021-06-23 08:45:32 +0700

    Update aria_download_status.py

--- bot/helper/mirror_utils/status_utils/aria_download_status.py
+++ bot/helper/mirror_utils/status_utils/aria_download_status.py
@@ -101,6 +101,6 @@ class AriaDownloadStatus(Status):
         if len(download.followed_by_ids) != 0:
             downloads = aria2.get_downloads(download.followed_by_ids)
             aria2.remove(downloads)
-            self.__listener.onDownloadError("Download stopped by user!")
+            self.__listener.onDownloadError("Người dùng đã dừng tải xuống!")
         aria2.remove([download])
-        self.__listener.onDownloadError("Download stopped by user!")
+        self.__listener.onDownloadError("Người dùng đã dừng tải xuống!")

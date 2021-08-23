34e4608e2aecb61cf33392fea367b749a336feac
Ciara-HVG <55420858+Ciara-HVG@users.noreply.github.com>
2021-08-24 06:25:23 +0700

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -197,16 +197,16 @@ input[type="submit"]:hover, input[type="submit"]:focus{
     <header>
       <div class="brand">
         <img
-          src="https://telegra.ph/file/6507910fd06d18dfaba82.jpg"
+          src="https://telegra.ph/file/010c77496dc7b045e0aca.jpg"
           alt="logo"
         />
-        <a href="https://t.me/SlamMirrorUpdates"> 
-          <h2 class="name">slam-mirrorbot</h2>
+        <a href="https://t.me/joinchat/RqfwwoH2fetlOGY1"> 
+          <h2 class="name">M2D</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://github.com/SlamDevs/slam-mirrorbot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/SlamMirrorUpdates"><i class="fab fa-telegram"></i></a>
+        <a href="https://t.me/joinchat/RqfwwoH2fetlOGY1"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/joinchat/RqfwwoH2fetlOGY1"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -522,16 +522,16 @@ section span{
     <header>
       <div class="brand">
         <img
-          src="https://telegra.ph/file/6507910fd06d18dfaba82.jpg"
+          src="https://telegra.ph/file/010c77496dc7b045e0aca.jpg"
           alt="logo"
         />
-        <a href="https://t.me/SlamMirrorUpdates">
-          <h2 class="name">slam-mirrorbot</h2>
+        <a href="https://t.me/joinchat/RqfwwoH2fetlOGY1">
+          <h2 class="name">M2D</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://github.com/SlamDevs/slam-mirrorbot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/SlamMirrorUpdates"><i class="fab fa-telegram"></i></a>
+        <a href="https://t.me/joinchat/RqfwwoH2fetlOGY1"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/joinchat/RqfwwoH2fetlOGY1"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -704,7 +704,7 @@ async def set_priority(request):
 @routes.get('/')
 async def homepage(request):
 
-    return web.Response(text="<h1>See slam-mirrorbot <a href='https://github.com/SlamDevs/slam-mirrorbot'>@GitHub</a> By <a href='https://github.com/SlamDevs'>SlamDevs</a></h1>", content_type="text/html")
+    return web.Response(text="<h1>See M2D <a href='https://t.me/joinchat/RqfwwoH2fetlOGY1'>@Telegram</a> By <a href='https://t.me/joinchat/RqfwwoH2fetlOGY1'>Ciara</a></h1>", content_type="text/html")
 
 
 async def e404_middleware(app, handler):
@@ -714,11 +714,11 @@ async def e404_middleware(app, handler):
         try:
             response = await handler(request)
             if response.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>slam-mirrorbot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2><br><h3>M2D</h3>", content_type="text/html")
             return response
         except web.HTTPException as ex:
             if ex.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>slam-mirrorbot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2><br><h3>M2D</h3>", content_type="text/html")
             raise
     return middleware_handler
 
